96cfe7606ab25ebae4660ce1fc597f0142eaaf62
Nikhil9945 <01fe20bec147@kletech.ac.in>
2024-02-05 16:54:20 +0530

    new files added till signin

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
-  return (
-    <h1>Profile</h1>
-  )
+  return <div>Profile</div>;
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function SignIn() {
-  return (
-    <h1>SignIn</h1>
-  )
+  return <div>SignIn</div>;
 }
