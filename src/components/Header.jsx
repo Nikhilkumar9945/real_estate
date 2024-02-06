e6e4c944a119d2113543516c3af6f570c6eceebe
Nikhilkumar9945 <pawar.nikhil0702@gmail.com>
2024-02-06 22:18:28 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,26 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-slate-300 shadow-md">
       <div className="flex justify-between">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sahand</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-purple-500">GrandView</span>
+            <span className="text-blue-400"> Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search... "
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64 hover:selection"
           />
           <FaSearch className="text-slate-600" />
         </form>
         <ul>
           <Link to="/">
-            <li className="hidden sm:inline tex t-slate-700 hover:underline">
-              {"   "}Home
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
             </li>
           </Link>
           <Link to="/about">
@@ -31,7 +31,7 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className=" text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
               {"   "}
               Sign in
             </li>
