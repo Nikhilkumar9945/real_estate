88ac551fc1e077179376c7300c34bb63bf09bb20
Nikhilkumar9945 <pawar.nikhil0702@gmail.com>
2024-02-15 00:06:10 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, passward } = req.body;
   const hashedPassward = bcryptjs.hashSync(passward, 10);
@@ -8,6 +9,23 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, passward } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassward = bcryptjs.compareSync(passward, validUser.passward);
+    if (!validUser) return next(errorHandler(401, "Wrong Crendential!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { passward: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
