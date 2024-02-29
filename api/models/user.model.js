d1accc6a932354f6ab3a3a32eb901c5b879db006
Nikhilkumar9945 <pawar.nikhil0702@gmail.com>
2024-02-29 18:50:52 +0530

    google created

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://imgs.search.brave.com/ZXmRUDhLKRexTPZ6IFmRVXwLle_bsYjYL3cN7CjwVDE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvcHJldmll/dy0xeC82NS81Ni91/bmtub3duLXBlcnNv/bi11c2VyLWljb24t/d2l0aC1hLWdpcmwt/dmVjdG9yLTQwMjI2/NTU2LmpwZw",
+    },
   },
   { timestamps: true }
 );
