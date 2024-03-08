9421f67b80b4f3ec3428573f346b252204b332e8
Nikhilkumar9945 <pawar.nikhil0702@gmail.com>
2024-03-09 00:06:58 +0530

    create user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,37 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
 export const test = (req, res) => {
     res.json({
         message: 'Api route is working !',
     });
 };
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only update your own account!'));
+    try {
+      if (req.body.passward) {
+        req.body.passward = bcryptjs.hashSync(req.body.passward, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            passward: req.body.passward,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+  
+      const { passward, ...rest } = updatedUser._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
